4cbe696f916810b8a55d9457a28101fc4760b09d
LAPTOP-B9CE46NT\frank <frank.lu@frog-jump.com>
2024-04-25 17:59:40 +0800

    React Material UI Tutorial - 4 - Button

--- src/pages/MuiButton.jsx
+++ src/pages/MuiButton.jsx
@@ -1,23 +1,54 @@
-import { Stack, Button } from '@mui/material'
-import React from 'react'
+import { Stack, Button } from "@mui/material";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import React from "react";
 
 export const MuiButton = () => {
     return (
         <Stack spacing={4}>
-            <Stack spacing={2} direction="row" alignItems="center" justifyContent="center">
+            <Stack spacing={2} direction="row" alignItems="center">
                 <Button variant="text">Hello World</Button>
                 <Button variant="contained">Hello World</Button>
                 <Button variant="outlined">Hello World</Button>
             </Stack>
-            
-            <Stack spacing={2} direction="row" alignItems="center" justifyContent="center">
-                <Button variant="contained" color="primary">Primary</Button>
-                <Button variant="contained" color="secondary">Secondary</Button>
-                <Button variant="contained" color="error">Error</Button>
-                <Button variant="contained" color="warning">Warning</Button>
-                <Button variant="contained" color="info">Info</Button>
-                <Button variant="contained" color="success">Success</Button>
+
+            <Stack spacing={2} direction="row" alignItems="center">
+                <Button variant="contained" color="primary">
+                    Primary
+                </Button>
+                <Button variant="contained" color="secondary">
+                    Secondary
+                </Button>
+                <Button variant="contained" color="error">
+                    Error
+                </Button>
+                <Button variant="contained" color="warning">
+                    Warning
+                </Button>
+                <Button variant="contained" color="info">
+                    Info
+                </Button>
+                <Button variant="contained" color="success">
+                    Success
+                </Button>
+            </Stack>
+
+            <Stack spacing={2} direction="row" alignItems="center">
+                <Button variant="contained" size="small">
+                    Small
+                </Button>
+                <Button variant="contained" size="medium">
+                    Medium
+                </Button>
+                <Button variant="contained" size="large">
+                    Large
+                </Button>
+            </Stack>
+
+            <Stack spacing={2} direction="row" alignItems="center">
+                <Button variant="contained" startIcon={<AccountCircleIcon />}>
+                    Account
+                </Button>
             </Stack>
         </Stack>
-    )
-}
+    );
+};
